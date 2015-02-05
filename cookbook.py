fe44d9b19bdac0bc0cb1966974b771bd317c7e53
vsheremet <vsheremet@whoi.edu>
2015-02-05 16:54:45 -0500

    Update cookbook.py

--- cookbook.py
+++ cookbook.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import datetime
 t=datetime.datetime.strptime('2014-09-10 11:08:15.21','%Y-%m-%d %H:%M:%S.%f') 
 
@@ -6,4 +8,10 @@ import dateutil.parser
 S1='2013-04-21T15:14:159265Z'                                        
 t1=dateutil.parser.parse(S1)                                           
 S2='04/21/2013 15:14'                                        
-t2=dateutil.parser.parse(S2)                                           
+t2=dateutil.parser.parse(S2)      
+
+a=np.array([11.,12.,13.,14.,15.])
+for idx, val in enumerate(a):
+    print(idx,val)
+    
+#    if __name__ == "__main__":
